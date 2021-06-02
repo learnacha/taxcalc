83db6744c942654b35fabbb66c3e6625ad0949a2
Sri Hari Acha <srihari.acha@emirates.com>
2021-06-02 15:06:59 +0400

    update env config

--- main.js
+++ main.js
@@ -30,11 +30,15 @@ var route = express.Router();
 // All our services are under the /api context
 app.use('/api', route);
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
+
 // Start defining routes for our app/microservice
 
 // A route that dumps hostname information from pod
 route.get('/', function(req, res) {
-    res.send('Hi! I am running on host -> ' + hostname + '\n');
+    res.send('Hi! I am running on host -> ' + hostname + '\n' + 'config' + process.env.MY_CONFIG);
 });
 
 // This route handles tax calculation for our service
