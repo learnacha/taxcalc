b869f492f7fc1f0ecd348c3689827acaefad9414
Sri Hari Acha <srihari.acha@emirates.com>
2021-06-03 14:32:37 +0400

    reading config from container

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose   = require('mongoose'),
     os = require('os'),
+    fs = require('fs'),
     hostname = os.hostname();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,11 +35,35 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   });
 
+  const loadFile = () => {
+    fs.readFile('/root/.master/master-config.json', 'utf8', (err, data) => {
+
+        if (err) {
+            console.log(`Error reading file from disk: ${err}`);
+        } else {
+    
+            // parse JSON string to JSON object
+            const databases = JSON.parse(data);
+
+            return databases;
+    
+            // // print all databases
+            // databases.forEach(db => {
+            //     console.log(`${db.name}: ${db.type}`);
+            // });
+        }
+    
+    });
+  }
+
 // Start defining routes for our app/microservice
 
 // A route that dumps hostname information from pod
 route.get('/', function(req, res) {
-    res.send('Hi! I am running on host -> ' + hostname + '\n' + 'config' + process.env.MY_CONFIG);
+    const data = loadFile();
+    // res.send('Hi! I am running on host -> ' + hostname + '\n' + 'config' + process.env.MY_CONFIG);
+    res.send('data', JSON.stringify(data));
+    res.send('data', JSON.parse(data));
 });
 
 // This route handles tax calculation for our service
